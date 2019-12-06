cc127c266d655fe2b859974908a061143c26724b
Cho Jangyeon <whwkd11010@naver.com>
2019-12-06 16:28:03 +0900

    Add JsonTest code and test file
    Junit test code implemented based on XmlTreeBuilderTest

--- jsoup-master/src/main/java/org/jsoup/examples/JsonTest.java
+++ jsoup-master/src/main/java/org/jsoup/examples/JsonTest.java
@@ -1,66 +1,53 @@
 package org.jsoup.examples;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.util.Map;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.parser.JsonTreeBuilder;
 import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
 
 public class JsonTest {
 
 	public static void main(String[] args) {
+		/*
 		String jsonFile = "{\n" + 
-				"   \"data\": [\n" + 
-				"      {\n" + 
-				"         \"id\": \"X12\",\n" + 
-				"         \"from\": {\n" + 
-				"            \"name\": \"John Kennedy\", \"id\": \"1458633\"\n" + 
-				"         },\n" + 
-				"         \"message\": \"JSON is much easier and better than XML\",\n" + 
-				"         \"actions\": [\n" + 
-				"            {\n" + 
-				"               \"name\": \"Comment\",\n" + 
-				"               \"link\": \"http://www.facebook.com/X999/posts/Y999\"\n" + 
-				"            },\n" + 
-				"            {\n" + 
-				"               \"name\": \"Like\",\n" + 
-				"               \"link\": \"http://www.facebook.com/X999/posts/Y999\"\n" + 
-				"            }\n" + 
-				"         ],\n" + 
-				"         \"type\": \"status\",\n" + 
-				"         \"created_time\": \"2010-08-02T22:27:44+0000\",\n" + 
-				"         \"updated_time\": \"2010-08-02T22:27:44+0000\"\n" + 
+				"   \"data\": {\n" + 
+				"      \"id\": \"X12\",\n" + 
+				"      \"from\": {\n" + 
+				"         \"name\": \"John Kennedy\", \"id\": \"1458633\"\n" + 
 				"      },\n" + 
-				"      {\n" + 
-				"         \"id\": \"X45\",\n" + 
-				"         \"from\": {\n" + 
-				"            \"name\": \"Anna Smith\", \"id\": \"12587521\"\n" + 
-				"         },\n" + 
-				"         \"message\": \"On my way to write my very first JSON snippet\",\n" + 
-				"         \"actions\": [\n" + 
-				"            {\n" + 
-				"               \"name\": \"Comment\",\n" + 
-				"               \"link\": \"http://www.facebook.com/X998/posts/Y998\"\n" + 
-				"            },\n" + 
-				"            {\n" + 
-				"               \"name\": \"Like\",\n" + 
-				"               \"link\": \"http://www.facebook.com/X998/posts/Y998\"\n" + 
-				"            }\n" + 
-				"         ],\n" + 
-				"         \"type\": \"status\",\n" + 
-				"         \"created_time\": \"2010-08-02T25:27:44+0000\",\n" + 
-				"         \"updated_time\": \"2010-08-02T25:27:44+0000\"\n" + 
-				"      }\n" + 
-				"   ]\n" + 
+				"      \"message\": \"JSON is much easier and better than XML\"\n" + 
+				"   }\n" + 
 				"}";
+		String simpleJson = "{ \"test\" : \"data\", \"test2\" : 2}";
 		Document doc = Jsoup.parse(jsonFile, "", Parser.jsonParser());
-		System.out.println("Debuging start");
-		Elements e = doc.select("data");
-		System.out.println(e.get(0));
-		System.out.println("Debuging End");
+		System.out.println(doc.html());*/
+		
+		File jsonFile;
+		try {
+			jsonFile = new File(JsonTreeBuilder.class.getResource("/html/json-test.json").toURI());
+	        InputStream inStream = new FileInputStream(jsonFile);
+	        Document doc = Jsoup.parse(inStream, null, "http://foo.com", Parser.jsonParser());
+			System.out.println("Debuging start");
+			System.out.println(doc.html());
+			System.out.println("Debuging End");
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+
 	}
 }
 
